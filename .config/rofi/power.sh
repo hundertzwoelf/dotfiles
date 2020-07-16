7e3e6eef5ed0a68ac3651c8a5de4e2311212a7d1
Patrick Hetco <patrick.hetco@gmail.com>
2020-07-16 09:33:06 +0200

    moved around stuff in i3 config, added power menu

---
+++ .config/rofi/power.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OPTIONS="\tLogout\n\tSuspend\n\tShutdown\n\tReboot"
+
+option=`echo -e $OPTIONS | awk '{print $1}' | tr -d '\r\n\t'`
+if [ "$@" ]
+then
+    case $@ in
+        *Logout)
+            i3-msg exit
+            ;;
+        *Suspend)
+            systemctl suspend
+            ;;
+        *Shutdown)
+            shutdown now
+            ;;
+        *Reboot)
+            reboot
+            ;;
+    esac
+else
+    echo -e $OPTIONS
+fi
