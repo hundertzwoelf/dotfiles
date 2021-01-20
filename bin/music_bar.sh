eddcf6955411b2e4eaa03ffcf3ac86576eb94f67
Patrick Hetco <patrick.hetco@gmail.com>
2021-01-20 11:16:17 +0100

    Fixed: Closed players were still shown

--- bin/music_bar.sh
+++ bin/music_bar.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# create player index file and set it to nil
 [ -f /tmp/music.id ] || echo 0 > /tmp/music.id
 
+# gather player list, total number, current index and current player
 players=( $(playerctl -l 2> /dev/null) )
 total=( $(playerctl -l  2> /dev/null | wc -l) )
 i=( $(cat /tmp/music.id) )
 player=${players[$i]}
 
+# exit if total is nil
+[ $total -eq 0 ] && echo && exit
+
 # skip stopped players (playerctl retains stopped chromium instances)
 # same code as for switch, maybe reuse code ~TODO?
 if [ $(playerctl status --player $player) == "Stopped" ] ; then
     let new="($i+1) % total"
-    player=${players[$new]}$
+    player=${players[$new]}
     echo $new > /tmp/music.id
     exit
 fi
 
-# not sure if needed anymore
-# let last=$(( $total - 1 ))
-# [[ $i -gt $last ]] && echo 0 > /tmp/music.id
+# set the index back to 0
+let last=$(( $total - 1 ))
+[[ $i -gt $last ]] && echo 0 > /tmp/music.id
 
 # handle command line options for switching, play-pause and focussing the ws
 for option in "$@"
@@ -26,7 +31,7 @@ do
     case $option in
         --switch)
             let new="($i + 1) % $total"
-            player=${players[$new]}$
+            player=${players[$new]}
             echo $new > /tmp/music.id
             exit
             ;;
